10eca026da004c6a00d326ca6c59eedb8d33ad07
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-09-16 20:22:39 +0000

    aula sobre o tko2. 2h

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,1 +1,28 @@
-print("minha toalha é rosa")
+class  Towel:
+    def __init__(self, color: str, size: str):  #construtor
+        self.color: str = color #atributos
+        self.size: str = size
+        self.wetness: int = 0
+#como são criada
+
+    def __str__(self):
+        return f"color:{self.color}, tam:{self.size}, hum:{self.wetness}" 
+
+
+
+#referencia
+towel = Towel("green", "G") #objeto
+toalha = Towel ("red", "p") 
+outra = toalha
+mais_outras = toalha
+toalha.color ="blue"
+outra.color = "blue"
+
+
+print(towel.color) #vermelha
+towel.color = "white"
+print(towel.color)#branco
+print(towel.size)
+print(towel.wetness)
+
+
