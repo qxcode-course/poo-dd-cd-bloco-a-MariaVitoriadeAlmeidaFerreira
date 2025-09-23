8d4abf394e23480d9f430458f07dc210277c5542
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-09-23 20:17:49 +0000

    indo terminar local 4h

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,28 +1,56 @@
-class  Towel:
+class  Towel: #this
     def __init__(self, color: str, size: str):  #construtor
         self.color: str = color #atributos
         self.size: str = size
         self.wetness: int = 0
 #como são criada
 
-    def __str__(self):
-        return f"color:{self.color}, tam:{self.size}, hum:{self.wetness}" 
+    def dry(self, amount: int ) -> None:
+        self.wetness += amount
+        if self.wetness >= self.getMaxWetness():
+            print ("toalha encharcada")
+            self.wetness = self.getMaxWetness()
+
+    
+
+    def getMaxWetness(self) -> int:
+        #maximo de agua
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20
+        if self.size == "G":
+            return 30
+        return 0
 
 
+    def __str__(self): #toString
+            return f"color:{self.color}, tam:{self.size}, hum:{self.wetness}" 
+    
 
-#referencia
-towel = Towel("green", "G") #objeto
-toalha = Towel ("red", "p") 
-outra = toalha
-mais_outras = toalha
-toalha.color ="blue"
-outra.color = "blue"
+   
+def main():
+  toalha = Towel("", "") #3. objeto manipulado
+  while True: #4 loop infinito
+      line: str = input()
+      args: list[str] = line.split(" ")
+      if args[0] == "end":
+        break
+      elif args[0] == "new":
+          color = args[1]
+          size = args[2]
+          toalha = Towel(color, size)
 
+      elif args[0] == "show":
+          print(toalha)
+      elif args[0] == "dry":
+          amount: int = int(args[1])
+          toalha.dry(amount)
+      else:
+          print("fail")
 
-print(towel.color) #vermelha
-towel.color = "white"
-print(towel.color)#branco
-print(towel.size)
-print(towel.wetness)
+      
+main()
+
 
 
